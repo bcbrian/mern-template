2248d029033ccf5072b6675f6543bd1c30456cda
Brian Bartholomew <me@bcbrian.com>
2019-10-15 21:42:01 -0500

    some cleanup...

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import jwt_decode from "jwt-decode";
-import setAuthToken from "./utils/setAuthToken";
-
-import { setCurrentUser, logoutUser } from "./actions/authActions";
-
-// import { Provider } from "react-redux";
-
-import store from "./store";
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
@@ -16,25 +8,24 @@ import Login from "./components/auth/Login";
 import PrivateRoute from "./components/private-route/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
 
-import {Auth} from './auth/auth'
+import { Auth } from "./auth/auth";
 
 import "./App.css";
 
-
 export default function() {
-    return (
-      <Auth>
-        <Router>
-          <div className="App">
-            <Navbar />
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-            <Switch>
-              <PrivateRoute exact path="/dashboard" component={Dashboard} />
-            </Switch>
-          </div>
-        </Router>
-      </Auth>
-    );
-  }
+  return (
+    <Auth>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Switch>
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          </Switch>
+        </div>
+      </Router>
+    </Auth>
+  );
+}
