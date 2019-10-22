625df65a4ed3557c5358057b0017b12a4c19f1ea
Brian Bartholomew <me@bcbrian.com>
2019-10-22 11:02:11 -0500

    I think i can

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.use(
 );
 app.use(bodyParser.json());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+ }
+
 // DB Config
 const db = require("./config/keys").mongoURI;
 
@@ -37,6 +41,10 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use("/api/v1/games", games);
+
+router.use((req, res) =>
+ res.sendFile(path.join(__dirname, "../client/build/index.html"))
+);
 
 const port = process.env.PORT || 5000;
 
