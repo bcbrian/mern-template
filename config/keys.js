ff7e6f0621839dd64565bb2e592c061310779d7f
Brian Bartholomew <me@bcbrian.com>
2019-10-22 11:40:03 -0500

    fixed

--- config/keys.js
+++ config/keys.js
@@ -2,3 +2,5 @@ module.exports = {
   mongoURI: process.env.MONGO_URI || "mongodb://localhost/tictacdb",
   secretOrKey: "secret"
 };
+
+console.log("################# >>>>>>>>>>", process.env.MONGO_URI)
