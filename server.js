e1463fd5054ca4e120a0e45370515a099549694a
Brian Bartholomew <me@bcbrian.com>
2019-10-23 10:14:02 -0500

    test

--- server.js
+++ server.js
@@ -42,8 +42,7 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/v1/games", games);
 
-const router = express.Router();
-router.use("*", (req, res) =>
+app.use("*", (req, res) =>
  res.sendFile(path.join(__dirname, "../client/build/index.html"))
 );
 
